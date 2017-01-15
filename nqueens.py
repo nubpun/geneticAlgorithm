a802f8737d81d61fb3d2ea649db4c8429f302e87
Mayorov Maxim <nub.pun@yandex.ru>
2017-01-15 16:10:47 +0500

    start dev

---
+++ nqueens.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+class Solver_8_queens:
+    
+    def __init__(self, pop_size=1000, cross_prob=0.11, mut_prob=0.05):
+        pass
+    '''
+    Dummy method representing proper interface
+    '''
+    def solve(self, min_fitness=0.9, max_epochs=100):
+        best_fit=None
+        epoch_num=None
+        visualization=None
+        return best_fit, epoch_num, visualization
